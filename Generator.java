a9f22351e4958d2dd0dbc70a9cbffd43089859e9
FMammaddli <fmammadli13936@ada.edu.az>
2026-02-09 01:10:51 +0400

    Attempted to add populate function

--- Generator.java
+++ Generator.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
+import java.util.Scanner;
 
 //You are required to implement a Java program that generates random numbers using different built-in Java random number generators and analyzes the generated data using descriptive statistics.
 
@@ -13,6 +13,24 @@ public class Generator
     // number generators.
     ArrayList<Double> populate(int n, int randNumGen) 
     {
+        ArrayList<Double> al = new ArrayList<>();
+        Random rand = new Random();
         
+        //print 3 random numbers, store in the array list
+        for (int i = 1; i <=3; i++)
+        {
+            al.add(rand.nextDouble(0,1));
+        }
+
+        return al;
     }  
+
+
+    // Calculates the number of elements (n), mean, sample standard deviation, minimum, 
+    // and maximum, and returns the results in the following order:
+    // [n, mean, stddev, min, max]
+    ArrayList<Double> statistics(ArrayList<Double> randomValues)
+    {
+
+    }
 }
