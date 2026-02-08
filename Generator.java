9dcd913feff4edce54d780189020e0625c1e9b30
FMammaddli <fmammadli13936@ada.edu.az>
2026-02-09 00:21:09 +0400

    Initial Commit

---
+++ Generator.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+//You are required to implement a Java program that generates random numbers using different built-in Java random number generators and analyzes the generated data using descriptive statistics.
+
+public class Generator
+{
+
+
+    // Creates and returns an ArrayList of n random numbers using one of the three random
+    // number generators.
+    ArrayList<Double> populate(int n, int randNumGen) 
+    {
+        
+    }  
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+
+--->                    Farid Mammadli 
+--->                     Assignment 1
+--->        Spring 2026 Object Oriented Analysis&Design (CSCI-3612 - 20966)
+
+*/
+
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+        Generator g = new Generator();
+        g.execute();   
+    }
+}
